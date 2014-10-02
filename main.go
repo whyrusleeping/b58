c77fbb5fe1ef60aaa8f88f37ebc3b8868b735d8e
Jeromy <jeromyj@gmail.com>
2014-10-02 04:03:46 +0000

    first hack at a cmd line base58 tool

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/jbenet/go-base58"
+	"github.com/spf13/cobra"
+)
+
+var b58Cmd = &cobra.Command{
+	Use:   "b58",
+	Short: "",
+	Long:  "",
+	Run:   b58CmdFunc,
+}
+
+var encCmd = &cobra.Command{
+	Use:   "enc",
+	Short: "",
+	Long:  "",
+	Run:   encodeFunc,
+}
+
+var decCmd = &cobra.Command{
+	Use:   "dec",
+	Short: "",
+	Long:  "",
+	Run:   decodeFunc,
+}
+
+func b58CmdFunc(c *cobra.Command, inp []string) {
+	c.Help()
+}
+
+func encodeFunc(c *cobra.Command, inp []string) {
+	i, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Print(base58.Encode(i))
+}
+
+func decodeFunc(c *cobra.Command, inp []string) {
+	if len(inp) > 0 {
+		for _, s := range inp {
+			fmt.Println(string(base58.Decode(s)))
+		}
+	} else {
+		i, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		os.Stdout.Write(base58.Decode(string(i)))
+	}
+}
+
+func main() {
+	b58Cmd.AddCommand(encCmd, decCmd)
+	b58Cmd.Execute()
+}
